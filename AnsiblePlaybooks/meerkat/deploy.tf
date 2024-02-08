75ebe599238b0177400cda95f9aee5aa21dd5109
Chris Green <138027265+Chris-green-stfc@users.noreply.github.com>
2024-02-08 15:51:54 +0000

    Fixed SSH connection race condition
    Updates machine and waits for SSH connection seccuessfully

--- AnsiblePlaybooks/meerkat/deploy.tf
+++ AnsiblePlaybooks/meerkat/deploy.tf
@@ -16,8 +16,6 @@ provider "openstack" {
   cloud = "openstack"	# Uses the section called “openstack” from our app creds
 }
 
-
-
 locals {
   my_product = {for val in setproduct(var.flavor_name, var.image_name):
     "${val[0]}-${val[1]}" => val}
@@ -36,22 +34,8 @@ resource "openstack_compute_instance_v2" "Instance" {
   metadata = {
     group = var.VM_group
   }
-<<<<<<< HEAD
-    provisioner "local-exec" {
-         command = "ANSIBLE_HOST_KEY_CHECKING=False ansible -m wait_for_connection -i staging-openstack.yaml ${self.name}"
-    }
-    provisioner "local-exec" {
-         command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i staging-openstack.yaml -l ${self.name} meerkat.yaml --tags ${var.tags}"
-    }
-=======
-#    provisioner "local-exec" {
-#         command = "ANSIBLE_HOST_KEY_CHECKING=False ansible -m wait_for_connection -i staging-openstack.yaml ${self.name}"
-#    }
-#    provisioner "local-exec" {
-#         command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i staging-openstack.yaml -l ${self.name} meerkat.yaml --tags storage"
-#    }
->>>>>>> Added capabilities to spin up and attach volumes
 }
+
 
 resource "openstack_blockstorage_volume_v3" "volumes" {
   count = length(local.my_product)
@@ -60,7 +44,6 @@ resource "openstack_blockstorage_volume_v3" "volumes" {
   description = "volume for benchmark testing"
   size = 15
 }
-
 
 locals {
   vm_ids = [ for val in openstack_compute_instance_v2.Instance : val.id]
@@ -74,29 +57,9 @@ resource "openstack_compute_volume_attach_v2" "vol_attach" {
   volume_id = local.volume_ids[count.index]
 
      provisioner "local-exec" {
-        command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i staging-openstack.yaml -l ${local.vm_names[count.index]} /home/diz41711/cloud-ops-tools/AnsiblePlaybooks/meerkat/meerkat.yaml --tags storage"
+        command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i staging-openstack.yaml -l ${local.vm_names[count.index]} /home/diz41711/cloud-ops-tools/AnsiblePlaybooks/meerkat/meerkat.yaml" #--tags storage"
    }
 }
-
-#resource "openstack_compute_volume_attach_v2" "vol_attach" {
-#  count = length(openstack_compute_instance_v2.Instance)
-#  instance_id = local.vm_ids[count.index]
-#  volume_id = local.volume_ids[count.index]
-#}
-
-#locals {
-#  vm_names = [ for val in openstack_compute_instance_v2.Instance: val.id]
-#}
-
-#resource "ansible_playbook" "playbook" {
-#  playbook = "/home/diz41711/cloud-ops-tools/AnsiblePlaybooks/meerkat/meerkat.yaml"
-#  name = "meerkat-l3.tiny-ubuntu-focal-20.04-nogui"
-#  groups = [ var.VM_group ]
-#  tags = [ "storage" ]
-#  verbosity = 3
-#}
-
-
 
 
 
