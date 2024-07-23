94d65e38e310f3f6e5df96fd0e475ed191ea2632
David C <david.corner@stfc.ac.uk>
2024-07-23 15:37:28 +0100

    ip checker added ip checker as well as a few unit tests to accompany theses changes the ip checker should check dns and ips for resolvability and print the result to the command line to the user

--- reverseforwardrecord/get_dns_record.py
+++ reverseforwardrecord/get_dns_record.py
@@ -1,8 +1,10 @@
-from typing import List, Dict
-from openpyxl import load_workbook
+import sys
+from typing import List, Dict, Tuple
 from functools import reduce
 import socket
 import pandas as pd
+import argparse
+import subprocess
 
 """
 read from 2 txt files one containing dns name and another containing ip address
@@ -15,48 +17,48 @@ otherwise have to set it up with manual user input
 """
 
 
-def create_file():
+def create_file(
+        out_filepath: str = "output.txt"
+):
     """
     makes a txt file for the results to be returned into
     :return: output.txt
     """
-    f = open("ignored/output.txt", "w")
-    f.close()
+    with open(out_filepath, "w"):
+        pass
 
 
 def read_from_netbox(
-        netbox_filepath: str, fqdn_column: int = 8, idrac_ip_column: int = 10
+        netbox_filepath: str, fqdn_column: str = "FQDN", idrac_ip_column: str = "IDRAC IP"
 ) -> List[Dict]:
     """
     reads from netbox file, in a xlsx format
     :param netbox_filepath: path for the netbox info
     :param fqdn_column: full qualified domain name column in netbox file
-    :param idrac_ip_column: idrac ip column in netbox file
+    :param idrac_ip_column: idrac ip column name in netbox file
     :return: a list of dictionaries containing fqdn and idrac_ip pairs
     """
 
-    workbook = load_workbook(filename=netbox_filepath)
+    sheetnames = pd.ExcelFile(netbox_filepath).sheet_names
+
     # get all sheets on the XLSX file.
-    sheets = workbook.sheetnames
     workbook_info = []
 
-    for i, sheet in enumerate(sheets, 1):
-        for j, row in enumerate(workbook[sheet]):
-            if j == 0:
-                # ignore headers
-                continue
+    for i, sheet in enumerate(sheetnames, 1):
+        workbook = pd.read_excel(netbox_filepath, sheet_name=sheet, engine='openpyxl').to_dict()
 
-            try:
-                val = {
-                    "fqdn": row[fqdn_column].value,
-                    "idrac_ip": row[idrac_ip_column].value,
+        if fqdn_column not in workbook or idrac_ip_column not in workbook:
+            raise RuntimeError(
+                f"Error: you've provided an excel sheet: (no. {i}) which does not contain one or more of these "
+                f"following column in workbook: {fqdn_column}, {idrac_ip_column}"
+            )
+        for key, fqdn_val in workbook[fqdn_column].items():
+            workbook_info.extend([
+                {
+                    "fqdn": fqdn_val,
+                    "idrac_ip": workbook[idrac_ip_column][key]
                 }
-            except IndexError as exp:
-                raise RuntimeError(
-                    f"Error: you've provided an excel sheet: (no. {i}) which does not contain a value in "
-                    f"either the fqdn column {fqdn_column} or idrac_ip column {idrac_ip_column}"
-                ) from exp
-            workbook_info.append(val)
+            ])
     return workbook_info
 
 
@@ -66,7 +68,7 @@ def parse_netbox_info(
     """
     Parse info from netbox and create corresponding hypervisor fqdn and ip value
     :param netbox_info: list of dictionaries holding fqdn and idrac_ip
-    :param reverse_order: use reverse or forward order / if true use forward if false use reverse
+    :param reverse_order: use reverse or forward order / if true use forward if  false use reverse
     :return: list of dictionaries which will contain hypervisor fqdn and ip keys-value pairs
     """
     parsed_info = []
@@ -96,6 +98,7 @@ def write_output(
     :param reverse_order: use reverse or forward order / if true use forward if false use reverse
     :return: the text written into the file
     """
+    print(output_filepath)
     with open(output_filepath, "w", encoding="utf-8") as out_file:
         for item in parsed_info:
             if reverse_order:
@@ -104,15 +107,118 @@ def write_output(
                 out_file.write(f"{item['hypervisor']}\tIN A\t{item['ip_address']}\n")
 
 
-def main(
+def parse_args_dns(inp_args):
+    """
+    Parse command line args
+    :param inp_args: the arguments putted in via command line from cmd_args to inp_args
+    :return: the function according to the flags used
+    """
+    ap = argparse.ArgumentParser(description='option-selector')
+    ap.add_argument("input_filepath", type=argparse.FileType("r"))
+    ap.add_argument("output_filepath", default="output.txt", nargs="?", type=argparse.FileType("w"))
+    ap.add_argument("-r", "--reverse", default=False, help="if set reverses format if not set forwards format",
+                    action="store_true")
+    ap.add_argument("-c", "--check", default=False, help="check ips if they would work")
+    ap.add_argument("-d", "--fqdn-column-name", default="FQDN", help="set FQDN column name")
+    ap.add_argument("-i", "--idrac-ip-column-name", default="IDRAC IP", help="set IDRAC IP column name")
+    args, _ = ap.parse_known_args(inp_args)
+    return args
+
+
+def check_ip(reverse_order: bool = False,
+             output_filepath: str = "output.txt"):
+    """
+    checks for the ips and then appends them to lists to be checked on from later on in the script
+    :param reverse_order: reverse order tracks where to use the position in the function to be able
+    to correctly append the right string
+    :param output_filepath: filepath used to grab the ips from
+    :return: returns ips_found, dns_found
+    """
+    using_file = output_filepath
+    if not reverse_order:
+        position_dns = 0
+        position_ip = 2
+    else:
+        position_dns = 2
+        position_ip = 0
+    ips_found = []
+    dns_found = []
+    with open(using_file, 'r') as outfile:
+        for line in outfile.read().splitlines():
+            ips_found.append(line.split('\t')[position_ip])
+            dns_found.append(line.split('\t')[position_dns])
+    return ips_found, dns_found
+
+
+def is_reachable_ip(ip: str) -> bool:
+    """
+    used to ping an ip and see whether it is reachable or not
+    :param ip: converts ip to string
+    :return: returns boolean result
+    """
+    try:
+        result = subprocess.run(["ping", "-c", "1", ip], capture_output=True)
+        return result.returncode == 0
+    except Exception as e:
+        print(f"Error pinging IP {ip}: {e}")
+        return False
+
+
+def is_reachable_dns(dns_name: str) -> bool:
+    """
+    used to resolve a dns and see whether it is reachable or not
+    :param dns_name: converts dns to string
+    :return: returns boolean result
+    """
+    try:
+        socket.gethostbyname(dns_name)
+        return True
+    except socket.error as e:
+        print(f"Error resolving DNS {dns_name}: {e}")
+        return False
+
+
+def check_reachability(ips_found: List[str], dns_found: List[str]) \
+        -> Tuple[List[str], List[str], List[str], List[str]]:
+    """
+    checks whether the ips and dns names are reachable, using two previous functions and then
+    appending the results of those to the lists which are after displayed to the command line
+    :params ips_found: ips in a list to be tested
+    :params dns_found: dns names in a list to be tested
+    :params tuple_lists: lists that cant be changed
+    :return: all the ips found and dns names found that into 2 sections being reachable meaning
+    working ips or dns or unreachable which are out of use ips or dns that cannot be reached
+    """
+    reachable_ips = []
+    unreachable_ips = []
+    reachable_dns = []
+    unreachable_dns = []
+
+    for ip in ips_found:
+        if is_reachable_ip(ip):
+            reachable_ips.append(ip)
+        else:
+            unreachable_ips.append(ip)
+
+    for dns_name in dns_found:
+        if is_reachable_dns(dns_name):
+            reachable_dns.append(dns_name)
+        else:
+            unreachable_dns.append(dns_name)
+
+    return reachable_ips, unreachable_ips, reachable_dns, unreachable_dns
+
+
+def get_dns_record(
         netbox_filepath: str,
-        fqdn_column: int = 8,
-        idrac_ip_column: int = 10,
+        fqdn_column_name: str = "FQDN",
+        idrac_ip_column_name: str = "IDRAC IP",
         reverse_order: bool = False,
-        output_filepath: str = "output.txt",
-):
+        output_filepath: str = "output.txt", ):
     """
     this function will create a file for the output for dns records
+    also testing each of the ips and dns to see whether they are usable or not printing after
+    to display the result
     :param netbox_filepath: path for the netbox info
     :param fqdn_column: full qualified domain name column in netbox file
     :param idrac_ip_column: idrac ip column in netbox file
@@ -120,12 +226,32 @@ def main(
     :param output_filepath: file path for output
     :return: None
     """
-    create_file()
-    netbox_info = read_from_netbox(netbox_filepath, fqdn_column, idrac_ip_column)
+    create_file(output_filepath)
+    netbox_info = read_from_netbox(netbox_filepath, fqdn_column_name, idrac_ip_column_name)
     parsed_netbox_info = parse_netbox_info(netbox_info, reverse_order)
     write_output(parsed_netbox_info, output_filepath, reverse_order)
+    check_ip(reverse_order, output_filepath)
+
+
+def main():
+    cmd_args = parse_args_dns(sys.argv[1:])
+    get_dns_record(
+        cmd_args.input_filepath.name,
+        cmd_args.fqdn_column_name,
+        cmd_args.idrac_ip_column_name,
+        cmd_args.reverse,
+        cmd_args.output_filepath.name
+    )
+    ips_found, dns_found = check_ip(cmd_args.check)
+    reachable_ips, unreachable_ips, reachable_dns, unreachable_dns = check_reachability(ips_found, dns_found)
+    print(f"Reachable IPs: {reachable_ips}")
+    print(f"Unreachable IPs: {unreachable_ips}")
+    print(f"Reachable DNS: {reachable_dns}")
+    print(f"Unreachable DNS: {unreachable_dns}")
+
+
+# input filepath name, fqdn row name, idrac ip row name, -r or -f for order, output path or file
 
 
 if __name__ == "__main__":
-    netbox_filepath = "ignored/test.xlsx"
-    main(netbox_filepath, reverse_order=False)
+    main()
--- reverseforwardrecord/test_get_dns_record.py
+++ reverseforwardrecord/test_get_dns_record.py
@@ -1,13 +1,21 @@
+import unittest
 from unittest import mock
+from unittest.mock import patch, NonCallableMock, call, MagicMock, Mock
 import pytest
-from unittest.mock import patch, MagicMock, NonCallableMock
-from reverseforwardrecord.ignored.get_dns_record_personal import main, read_from_netbox, parse_netbox_info, write_output
+from get_dns_record import parse_args_dns, get_dns_record, read_from_netbox, parse_netbox_info, write_output, \
+    create_file, check_ip, check_reachability, is_reachable_ip, is_reachable_dns
 
 
 @patch("get_dns_record.read_from_netbox")
 @patch("get_dns_record.parse_netbox_info")
 @patch("get_dns_record.write_output")
-def test_main(mock_write_output, mock_parse_netbox_info, mock_read_from_netbox):
+@patch("get_dns_record.check_ip")
+@patch("get_dns_record.create_file")
+def test_get_dns_record(mock_create_file,
+                        mock_check_ip,
+                        mock_write_output,
+                        mock_parse_netbox_info,
+                        mock_read_from_netbox):
     """
     tests main function
     should call all appropriate functions
@@ -18,22 +26,26 @@ def test_main(mock_write_output, mock_parse_netbox_info, mock_read_from_netbox):
     mock_reverse_order = NonCallableMock()
     mock_output_filepath = NonCallableMock()
 
-    main(
+    # Set up return values for the mocks
+    mock_read_from_netbox.return_value = Mock()
+    mock_parse_netbox_info.return_value = Mock()
+
+    # Call the function under test
+    get_dns_record(
         mock_netbox_filepath,
         mock_fqdn_column,
         mock_idrac_ip_column,
         mock_reverse_order,
         mock_output_filepath,
     )
-    mock_read_from_netbox.assert_called_once_with(
-        mock_netbox_filepath, mock_fqdn_column, mock_idrac_ip_column
-    )
-    mock_parse_netbox_info.assert_called_once_with(
-        mock_read_from_netbox.return_value, mock_reverse_order
-    )
-    mock_write_output.assert_called_once_with(
-        mock_parse_netbox_info.return_value, mock_output_filepath, mock_reverse_order
-    )
+
+    # Assert that the functions were called with the correct arguments
+    mock_create_file.assert_called_once_with(mock_output_filepath)
+    mock_read_from_netbox.assert_called_once_with(mock_netbox_filepath, mock_fqdn_column, mock_idrac_ip_column)
+    mock_parse_netbox_info.assert_called_once_with(mock_read_from_netbox.return_value, mock_reverse_order)
+    mock_write_output.assert_called_once_with(mock_parse_netbox_info.return_value, mock_output_filepath,
+                                              mock_reverse_order)
+    mock_check_ip.assert_called_once_with(mock_reverse_order, mock_output_filepath)
 
 
 def test_parse_netbox_info_empty():
@@ -96,20 +108,37 @@ def test_parse_netbox_info_with_two_sets():
 def run_read_from_netbox_test_case_fixture():
     """A fixture for running read_from_netbox() with different inputs"""
 
-    @patch("get_dns_record.load_workbook")
+    @patch("get_dns_record.pd")
     def _run_read_from_netbox_test_case(
-            workbook_return, expected_return, mock_load_workbook
+            mock_sheet_names, read_excel_return, expected_return, mock_pd
     ):
         """function which runs read_from_netbox() test case"""
+        mock_pd.ExcelFile.return_value.sheet_names = mock_sheet_names
+
         mock_netbox_filepath = NonCallableMock()
-        mock_fqdn_column = 0
-        mock_idrac_ip_column = 1
-        mock_load_workbook.return_value = workbook_return
+        mock_fqdn_column = "FQDN"
+        mock_idrac_ip_column = "IDRAC IP"
+
+        mock_pd.read_excel.return_value.to_dict.side_effect = read_excel_return
 
         res = read_from_netbox(
             mock_netbox_filepath, mock_fqdn_column, mock_idrac_ip_column
         )
-        mock_load_workbook.assert_called_once_with(filename=mock_netbox_filepath)
+
+        mock_pd.ExcelFile.assert_called_once_with(mock_netbox_filepath)
+
+        mock_read_excel_calls = []
+        for name in mock_sheet_names:
+            mock_read_excel_calls.append(
+                call(mock_netbox_filepath, sheet_name=name, engine="openpyxl")
+            )
+            mock_read_excel_calls.append(
+                call().to_dict()
+            )
+
+        mock_pd.read_excel.assert_has_calls(mock_read_excel_calls)
+        assert mock_pd.read_excel.return_value.to_dict.call_count == len(mock_sheet_names)
+
         assert res == expected_return
 
     return _run_read_from_netbox_test_case
@@ -120,120 +149,110 @@ def test_read_from_netbox_with_one_sheet_one_row(run_read_from_netbox_test_case)
     tests read from netbox
     checks the load workbook and if it reads the sheets properly
     """
-    fqdn_row1_item = MagicMock()
-    fqdn_row1_item.value = "fqdn1"
+    mock_sheet_names = ["Sheet1"]
 
-    idrac_ip_row1_item = MagicMock()
-    idrac_ip_row1_item.value = "idrac_ip1"
-
-    workbook_return = {
-        "sheet1": [[], [fqdn_row1_item, idrac_ip_row1_item]],
-    }
+    read_excel_return = [{
+        "FQDN": {1: "fqdn1"},
+        "IDRAC IP": {1: "idrac_ip1"}
+    }]
 
     expected_return = [{"fqdn": "fqdn1", "idrac_ip": "idrac_ip1"}]
 
-    run_read_from_netbox_test_case(workbook_return, expected_return)
+    run_read_from_netbox_test_case(mock_sheet_names, read_excel_return, expected_return)
 
 
 def test_read_from_netbox_no_sheets(run_read_from_netbox_test_case):
     """should return nothing if there is no sheets to be given"""
-    workbook_return = {}
+    mock_sheet_names = []
+    read_excel_return = []
     expected_return = []
-
-    run_read_from_netbox_test_case(workbook_return, expected_return)
+    run_read_from_netbox_test_case(mock_sheet_names, read_excel_return, expected_return)
 
 
 def test_read_from_netbox_one_sheet_many_rows(run_read_from_netbox_test_case):
     """checks return if there is one sheet to many rows working"""
 
-    fqdn_row1_item = MagicMock()
-    fqdn_row1_item.value = "fqdn1"
+    mock_sheet_names = ["Sheet1"]
 
-    idrac_ip_row1_item = MagicMock()
-    idrac_ip_row1_item.value = "idrac_ip1"
-
-    fqdn_row2_item = MagicMock()
-    fqdn_row2_item.value = "fqdn2"
-
-    idrac_ip_row2_item = MagicMock()
-    idrac_ip_row2_item.value = "idrac_ip2"
-
-    workbook_return = {
-        "sheet1": [
-            [],
-            [fqdn_row1_item, idrac_ip_row1_item],
-            [fqdn_row2_item, idrac_ip_row2_item],
-        ],
-    }
+    read_excel_return = [{
+        "FQDN": {1: "fqdn1", 2: "fqdn2"},
+        "IDRAC IP": {1: "idrac_ip1", 2: "idrac_ip2"}
+    }]
 
     expected_return = [
         {"fqdn": "fqdn1", "idrac_ip": "idrac_ip1"},
         {"fqdn": "fqdn2", "idrac_ip": "idrac_ip2"},
     ]
 
-    run_read_from_netbox_test_case(workbook_return, expected_return)
+    run_read_from_netbox_test_case(mock_sheet_names, read_excel_return, expected_return)
 
 
 def test_read_from_netbox_many_sheet_many_rows(run_read_from_netbox_test_case):
     """checks return if there is many sheet to many rows working"""
 
-    fqdn_row1_item = MagicMock()
-    fqdn_row1_item.value = "fqdn1"
+    mock_sheet_names = ["Sheet1", "Sheet2"]
 
-    idrac_ip_row1_item = MagicMock()
-    idrac_ip_row1_item.value = "idrac_ip1"
-
-    fqdn_row2_item = MagicMock()
-    fqdn_row2_item.value = "fqdn2"
-
-    idrac_ip_row2_item = MagicMock()
-    idrac_ip_row2_item.value = "idrac_ip2"
-
-    workbook_return = {
-        "sheet1": [
-            [],
-            [fqdn_row1_item, idrac_ip_row1_item],
-            [fqdn_row2_item, idrac_ip_row2_item],
-        ],
-        "sheet2": [
-            [],
-            [fqdn_row1_item, idrac_ip_row1_item],
-            [fqdn_row2_item, idrac_ip_row2_item],
-        ],
-    }
+    read_excel_return = [
+        {
+            "FQDN": {1: "fqdn1", 2: "fqdn2"},
+            "IDRAC IP": {1: "idrac_ip1", 2: "idrac_ip2"}
+        },
+        {
+            "FQDN": {1: "fqdn3", 2: "fqdn4"},
+            "IDRAC IP": {1: "idrac_ip3", 2: "idrac_ip4"}
+        }
+    ]
 
     expected_return = [
         {"fqdn": "fqdn1", "idrac_ip": "idrac_ip1"},
         {"fqdn": "fqdn2", "idrac_ip": "idrac_ip2"},
-        {"fqdn": "fqdn1", "idrac_ip": "idrac_ip1"},
-        {"fqdn": "fqdn2", "idrac_ip": "idrac_ip2"},
+        {"fqdn": "fqdn3", "idrac_ip": "idrac_ip3"},
+        {"fqdn": "fqdn4", "idrac_ip": "idrac_ip4"},
     ]
 
-    run_read_from_netbox_test_case(workbook_return, expected_return)
+    run_read_from_netbox_test_case(mock_sheet_names, read_excel_return, expected_return)
 
 
-@patch("get_dns_record.load_workbook")
-def test_read_from_netbox_index_error(mock_load_workbook):
+@patch("get_dns_record.pd")
+def test_read_from_netbox_FQDN_not_found(mock_pd):
     """
     tests read from netbox if there's a runtime error
     and the fqdn is not found
     """
+
+    mock_pd.ExcelFile.return_value.sheet_names = ["Sheet1"]
+
     mock_netbox_filepath = NonCallableMock()
-    fqdn_row1_item = MagicMock()
-    fqdn_row1_item.value = "fqdn1"
+    mock_fqdn_column = "FQDN"
+    mock_idrac_ip_column = "IDRAC IP"
 
-    idrac_ip_row1_item = MagicMock()
-    idrac_ip_row1_item.value = "idrac_ip1"
-    mock_load_workbook.return_value = {"sheet1": [fqdn_row1_item, idrac_ip_row1_item]}
-
-    with pytest.raises(RuntimeError):
-        read_from_netbox(mock_netbox_filepath, 0, 2)
+    mock_pd.read_excel.return_value.to_dict.side_effect = [{
+        "IDRAC IP": {1: "idrac_ip3", 2: "idrac_ip4"}
+    }]
 
     with pytest.raises(RuntimeError):
-        read_from_netbox(mock_netbox_filepath, 2, 1)
+        read_from_netbox(mock_netbox_filepath, "FQDN", "IDRAC IP")
 
 
-# STILL BEING DEVELOPED
+@patch("get_dns_record.pd")
+def test_read_from_netbox_IDRAC_IP_not_found(mock_pd):
+    """
+    tests read from netbox if there's a runtime error
+    and the IDRAC IP is not found
+    """
+
+    mock_pd.ExcelFile.return_value.sheet_names = ["Sheet1"]
+
+    mock_netbox_filepath = NonCallableMock()
+
+    mock_pd.read_excel.return_value.to_dict.side_effect = [{
+        "FQDN": {1: "fqdn3", 2: "fqdn4"}
+    }]
+
+    with pytest.raises(RuntimeError):
+        read_from_netbox(mock_netbox_filepath, "FQDN", "IDRAC IP")
+
+
 def test_write_output_reversed_order():
     """
     tests whether the write output function works in the reversed order format
@@ -262,3 +281,105 @@ def test_write_output_non_reversed_order():
 
     mock_open.assert_called_once_with(mock_output_filepath, "w", encoding="utf-8")
     mock_open.return_value.__enter__.return_value.write.assert_called_once_with("www\tIN A\t192.168.0.1\n")
+
+
+def test_create_file():
+    """
+    tests whether a file is created properly
+    """
+    mock_file_path = NonCallableMock()
+    with patch('builtins.open') as mock_open:
+        create_file(mock_file_path)
+    mock_open.assert_called_once_with(mock_file_path, "w")
+
+
+@patch("get_dns_record.argparse")
+def test_parse_args(mock_argparse):
+    """
+    tests that arguments have been parsed through argparse when using the script
+    using test arguments as a NCM (non-callable-mock) and calls and expected arguments
+    """
+    mock_expected_args = NonCallableMock()
+    mock_ap = mock_argparse.ArgumentParser.return_value
+
+    mock_argparse.ArgumentParser.return_value.parse_known_args.return_value = (mock_expected_args, NonCallableMock())
+    test_args = NonCallableMock()
+    res = parse_args_dns(test_args)
+
+    mock_argparse.ArgumentParser.assert_called_once_with(description='option-selector')
+    mock_ap.add_argument.assert_has_calls(
+        [
+            call('input_filepath', type=mock_argparse.FileType.return_value),
+            call('output_filepath', default='output.txt', nargs="?", type=mock_argparse.FileType.return_value),
+            call('-r', "--reverse", default=False, help="if set reverses format if not set forwards format",
+                 action="store_true"),
+            call("-c", "--check", default=False, help="check ips if they would work"),
+            call("-d", "--fqdn-column-name", default="FQDN", help="set FQDN column name"),
+            call("-i", "--idrac-ip-column-name", default="IDRAC IP", help="set IDRAC IP column name")
+        ]
+    )
+    mock_ap.parse_known_args.assert_called_once_with(test_args)
+    assert res == mock_expected_args
+
+
+def test_check_ip():
+    """
+    tests the function check ip accesses a file and is able to append to lists from that file
+    """
+    mock_file = NonCallableMock()
+    with patch('builtins.open') as mock_open:
+        check_ip(output_filepath=mock_file)
+    mock_open.assert_called_once_with(mock_file, "r")
+
+
+class TestIsReachableIp(unittest.TestCase):
+    @patch('subprocess.run')
+    def test_is_reachable_ip(self, mock_run):
+        """
+        tests that an ip is reachable
+        """
+        mock_run.return_value = MagicMock(returncode=0)
+        result = is_reachable_ip("8.8.8.8")
+        self.assertTrue(result)
+        mock_run.assert_called_once_with(["ping", "-c", "1", "8.8.8.8"], capture_output=True)
+
+
+class TestIsReachableDns(unittest.TestCase):
+    @patch('socket.gethostbyname')
+    def test_is_reachable_dns_success(self, mock_get_host_by_name):
+        """
+        tests that a dns is reachable
+        """
+        # Mock a successful DNS resolution
+        mock_get_host_by_name.return_value = '127.0.0.1'
+        result = is_reachable_dns("example.com")
+        self.assertTrue(result)
+        mock_get_host_by_name.assert_called_once_with("example.com")
+
+
+class TestCheckReachability(unittest.TestCase):
+    @patch('get_dns_record.is_reachable_ip')
+    @patch('get_dns_record.is_reachable_dns')
+    def test_check_reachability(self, mock_is_reachable_dns, mock_is_reachable_ip):
+        """
+        tests whether the ips found and dns found appended from a text file from a csv or xlsx
+        are reachable using the other functions and then tests using assert whether the responses
+        are true or not using side effects for exceptions
+        """
+        # Mocking is_reachable_ip responses
+        mock_is_reachable_ip.side_effect = [True, False, True]
+        # Mocking is_reachable_dns responses
+        mock_is_reachable_dns.side_effect = [False, True, False]
+
+        ips_found = ['192.168.1.1', '192.168.1.2', '192.168.1.3']
+        dns_found = ['example.com', 'example.org', 'example.net']
+
+        reachable_ips, unreachable_ips, reachable_dns, unreachable_dns = check_reachability(ips_found, dns_found)
+
+        self.assertEqual(reachable_ips, ['192.168.1.1', '192.168.1.3'])
+        self.assertEqual(unreachable_ips, ['192.168.1.2'])
+        self.assertEqual(reachable_dns, ['example.org'])
+        self.assertEqual(unreachable_dns, ['example.com', 'example.net'])
+
+        self.assertEqual(mock_is_reachable_ip.call_count, 3)
+        self.assertEqual(mock_is_reachable_dns.call_count, 3)
