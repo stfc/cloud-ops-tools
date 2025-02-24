a5f8e269072cb5d9709245f9c9cd245d52f332a1
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2025-02-24 15:27:56 +0000

    ENH: Add check for dev or prod FIP subnet
    Check the openstack openrc variables to see if the api endpoint is on dev or prod. This tells us which subnet ID to use

--- Weekly-Reporting/report.sh
+++ Weekly-Reporting/report.sh
@@ -3,11 +3,16 @@
 
 set -e
 
+if [[ $OS_AUTH_URL == *"dev"*  ]]; then
+  fip_subnet="0dc30001-edfb-4137-be76-8e51f38fd650"
+else
+  fip_subnet="5283f642-8bd8-48b6-8608-fa3006ff4539"
+fi
 openstack compute service list --long -f yaml > "compute_service_list_$(date +%F).yaml"
 
 openstack hypervisor list --long -f yaml > "hypervisor_list_$(date +%F).yaml"
 
-openstack ip availability show 0dc30001-edfb-4137-be76-8e51f38fd650 -f yaml > "floating_ip_list_$(date +%F).yaml"
+openstack ip availability show "${fip_subnet}" -f yaml > "floating_ip_list_$(date +%F).yaml"
 
 openstack server list --all-projects --limit -1 -f yaml > "server_list_$(date +%F).yaml"
 
